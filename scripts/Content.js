b12b2f9b44526853de035d69370a01d5fb613d28
AlwaysByrnes <141357497+AlwaysByrnes@users.noreply.github.com>
2024-04-04 17:24:13 -0400

    Add files via upload

---
+++ scripts/Content.js
@@ -0,0 +1,67 @@
+/* function rothIRACalculation() {
+    let input = parseFloat(document.getElementById('input').value);
+    let result = input * .06;
+    document.getElementById('result').innerText = "Roth: " + result;
+}
+ */
+
+/* const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function(){
+   
+    let inputCheck = parseFloat(document.getElementById('input-check').value);
+    let inputPercentage = parseFloat(document.getElementById('input-percentage').value);
+    let result = inputCheck * inputPercentage;
+    document.getElementById('result').innerText = "Roth: $" + (Math.round(result * 100) / 100).toFixed(2);
+
+    if(result < 0){
+        document.getElementById('result').innerText = "Error"
+    }else if (result === 0){
+        document.getElementById('result').innerText = "None"
+    }
+   
+})
+ */
+
+
+    const inputBtn = document.getElementById("input-btn")
+
+    inputBtn.addEventListener("click", function(){
+   
+        let inputCheck = parseFloat(document.getElementById('input-check').value);
+        let inputPercentage = parseFloat(document.getElementById('input-percentage').value);
+        let result = inputCheck * inputPercentage;
+        document.getElementById('result').innerText = "Roth: $" + (Math.round(result * 100) / 100).toFixed(2);
+    
+        if(result < 0){
+            document.getElementById('result').innerText = "Error"
+        }else if (result === 0){
+            document.getElementById('result').innerText = "None"
+        }
+       
+    })
+
+    const inputClear = document.getElementById("input-clear-btn");
+
+    inputClear.addEventListener("click", function(){
+        
+        document.getElementById('input-check').value = 0;
+        document.getElementById('input-percentage').value = 0;
+
+        document.getElementById('result').innerText  = "Roth: $" + 0;
+    })
+
+
+
+ 
+
+
+
+
+/* document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('link');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        hellYeah('xxx');
+    });
+}); */
